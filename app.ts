eb8d77275da3c24da13c844222b55c159e7d58ee
Rki0 <pky00823@gmail.com>
2023-12-02 17:21:50 +0900

    feat: add mongoose function to connect to DB

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-import authRouter from "./routes/authRouter";
+import AuthRouter from "./routes/authRouter";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,4 +14,15 @@ app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
 
-app.use("/api/auth", authRouter);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PSWD}@cluster0.imx8ztj.mongodb.net/`
+  )
+  .then(() => {
+    console.log("Connect Success!");
+  })
+  .catch((err) => {
+    console.log("Connect Fail...", err);
+  });
+
+app.use("/api/auth", AuthRouter);
