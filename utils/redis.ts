dec72e7a765d0b747ba1bc73ee05fcca7bb4d716
Rki0 <pky00823@gmail.com>
2023-12-13 09:43:29 +0900

    feat: change redis connect feature logic
    Before : try/catch statement is duplicated with on API to handling error.
    After : remove try/catch and only use 'on' API

--- utils/redis.ts
+++ utils/redis.ts
@@ -1,21 +1,15 @@
 import { createClient } from "redis";
 
 const connectRedis = async () => {
-  let redisClient;
-
-  try {
-    redisClient = await createClient({
-      password: process.env.REDIS_PSWD,
-      socket: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT as unknown as number,
-      },
-    })
-      .on("error", (err) => console.log("Redis Client Error", err))
-      .connect();
-  } catch (err) {
-    throw new Error("Redis 연결 실패");
-  }
+  const redisClient = await createClient({
+    password: process.env.REDIS_PSWD,
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT as unknown as number,
+    },
+  })
+    .on("error", (err) => console.log("Redis Client Error", err))
+    .connect();
 
   return redisClient;
 };
