d012aea9c948b17b8f4f5a6f501838dd32db3850
Rki0 <pky00823@gmail.com>
2023-12-25 17:58:14 +0900

    feat: extract commonly used middleware in comment router

--- routes/commentRouter.ts
+++ routes/commentRouter.ts
@@ -6,16 +6,10 @@ import authMiddleware from "../middlewares/authMiddleware";
 const router = Router();
 const commentController = new CommentController();
 
-router.post("/:feedId", authMiddleware, commentController.createComment);
-router.delete(
-  "/:feedId/:commentId",
-  authMiddleware,
-  commentController.deleteComment
-);
-router.patch(
-  "/:feedId/:commentId",
-  authMiddleware,
-  commentController.updateComment
-);
+router.use(authMiddleware);
+
+router.post("/:feedId", commentController.createComment);
+router.delete("/:feedId/:commentId", commentController.deleteComment);
+router.patch("/:feedId/:commentId", commentController.updateComment);
 
 export default router;
